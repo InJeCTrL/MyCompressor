1d8060ebab47b70bec1156e60bb6138a7b45bb98
InJeCTrL <dap1933@hotmail.com>
2018-08-18 20:54:16 +0800

    命令方式调用ffmpeg C#入门作

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace MyCompressor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 232);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(218, 32);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(141, 232);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox2_DrawItem);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(392, 146);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "暂停";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(392, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 30);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "停止";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "mp4视频压缩至H264、分辨率x360、比特率384";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(376, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "文件拖拽至左侧第一个控件\r\n双击项删除";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 276);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MyCompressor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+namespace MyCompressor
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("ntdll.dll")]
+        private static extern int NtResumeProcess([In] IntPtr processHandle);
+        [DllImport("ntdll.dll")]
+        private static extern int NtSuspendProcess([In] IntPtr processHandle);
+
+        Process proc;
+        Thread th;
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        private void ComPressor()
+        {
+            ProcessStartInfo procinfo = new ProcessStartInfo("ffmpeg.exe");
+            procinfo.UseShellExecute = false;
+            procinfo.CreateNoWindow = true;
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (File.Exists("E:\\A\\" + System.IO.Path.GetFileName((String)listBox1.Items[i])))//目标文件已存在
+                {
+                    if (DialogResult.No == MessageBox.Show(null, "文件已经存在于输出文件夹中\n是：覆盖生成，否：跳过此文件", "文件重复", MessageBoxButtons.YesNo))
+                    {
+                        listBox2.Items[i] = "Complete";
+                        continue;
+                    }
+                    else
+                        File.Delete("E:\\A\\" + System.IO.Path.GetFileName((String)listBox1.Items[i]));
+                }
+                procinfo.Arguments = "-i " + (String)listBox1.Items[i] + " -vcodec h264 -s 480*360 -b:v 384k " + "E:\\A\\" + System.IO.Path.GetFileName((String)listBox1.Items[i]);
+                proc = Process.Start(procinfo);
+                listBox2.Items[i] = "Compressing";
+                proc.WaitForExit();
+                proc.Close();
+                listBox2.Items[i] = "Complete";
+            }
+            listBox1.Enabled = true;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button2.Text = "暂停";
+            button3.Enabled = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button2.Text = "暂停";
+            button3.Enabled = true;
+            th = new Thread(ComPressor);
+            th.IsBackground = true;
+            th.Start();
+        }
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetData(DataFormats.FileDrop) != null)
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            String[] Files = (String[])e.Data.GetData(DataFormats.FileDrop);
+            int i;
+            int count = listBox1.Items.Count + 1;
+            for (i = 0; i < Files.Length; i++)
+            {
+                if (System.IO.Path.GetExtension(Files[i]) == ".mp4")
+                {
+                    listBox1.Items.Add(Files[i]);
+                    listBox2.Items.Add("Ready");
+                    
+                }
+            }
+        }
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            if (index != -1)
+            {
+                listBox1.Items.RemoveAt(index);
+                listBox2.Items.RemoveAt(index);
+            }
+        }
+        private void listBox2_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Brush setcolor = Brushes.Black;
+            if (e.Index >= 0)
+            {
+                string txt = listBox2.Items[e.Index].ToString();
+                if (txt.Equals("Ready"))
+                    e.Graphics.FillRectangle(Brushes.Red, e.Bounds);
+                else if (txt.Equals("Compressing"))
+                    e.Graphics.FillRectangle(Brushes.Yellow, e.Bounds);
+                else
+                    e.Graphics.FillRectangle(Brushes.Green, e.Bounds);
+                e.Graphics.DrawString(txt, e.Font, setcolor, e.Bounds, StringFormat.GenericDefault);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text.Equals("暂停"))
+            {
+                NtSuspendProcess(proc.Handle);
+                button2.Text = "继续";
+            }
+            else
+            {
+                NtResumeProcess(proc.Handle);
+                button2.Text = "暂停";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            th.Abort();
+            proc.Kill();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button2.Text = "暂停";
+            button3.Enabled = false;
+            listBox1.Enabled = true;
+            for (int i = 0; i < listBox2.Items.Count; i++)
+                if (listBox2.Items[i].ToString().Equals("Compressing"))
+                    listBox2.Items[i] = "Ready";
+        }
+    }
+}
