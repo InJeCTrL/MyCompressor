503afb967f6bec5784b6a7d5085f4713df360d1d
InJeCTrL <dap1933@hotmail.com>
2018-08-22 17:41:02 +0800

    增加状态栏进度显示、增加自定义输出目录、增加防止转换中关闭、增加FFmpeg进程的优先级设置、部分代码整理、完善注释

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -37,6 +37,12 @@
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -52,7 +58,7 @@
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(392, 144);
+            this.button2.Location = new System.Drawing.Point(392, 111);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(117, 30);
             this.button2.TabIndex = 3;
@@ -63,7 +69,7 @@
             // button3
             // 
             this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(392, 210);
+            this.button3.Location = new System.Drawing.Point(392, 147);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(117, 30);
             this.button3.TabIndex = 4;
@@ -76,9 +82,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(58, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(245, 12);
+            this.label1.Size = new System.Drawing.Size(251, 12);
             this.label1.TabIndex = 5;
-            this.label1.Text = "mp4视频压缩至H264、分辨率x360、比特率384";
+            this.label1.Text = "mp4视频压缩至H264、分辨率x360、比特率384k";
             // 
             // label2
             // 
@@ -127,11 +133,62 @@
             this.progressBar1.TabIndex = 8;
             this.progressBar1.Visible = false;
             // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(392, 219);
+            this.trackBar1.Maximum = 2;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(117, 45);
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.Tag = "";
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(390, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "优先级设置：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(392, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "标准";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(441, 251);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "高";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(480, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "实时";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(543, 276);
+            this.ClientSize = new System.Drawing.Size(532, 278);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -143,6 +200,9 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "MyCompressor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -159,6 +219,11 @@
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -23,19 +23,46 @@ namespace MyCompressor
 
         Process proc;//新建的ffmpeg进程
         Thread th;//用于创建并监视proc的线程
+        string OutputFolder;//指定输出文件夹
         int tDuration;//临时存储视频时长
         public Form1()
         {
             InitializeComponent();
-            CheckForIllegalCrossThreadCalls = false; 
+            CheckForIllegalCrossThreadCalls = false;//取消线程安全
+        }
+        private void Wnd_AllEnd()
+        {/*转换全部终止后对窗体中各控件的操作*/
+            this.Text = "MyCompressor";
+            button1.Enabled = true;
+            button1.Visible = true;
+            progressBar1.Visible = false;
+            button2.Enabled = false;
+            button2.Text = "暂停";
+            button3.Enabled = false;
+        }
+        private void SetPriorityByTrackBar()
+        {/*根据TrackBar的值设置FFmpeg进程优先级*/
+            switch (trackBar1.Value)
+            {/*根据trackBar获取优先级设置信息*/
+                case 0:
+                    proc.PriorityClass = ProcessPriorityClass.Normal;//设置FFmpeg进程为标准
+                    break;
+                case 1:
+                    proc.PriorityClass = ProcessPriorityClass.High;//设置FFmpeg进程为高
+                    break;
+                case 2:
+                    proc.PriorityClass = ProcessPriorityClass.RealTime;//设置FFmpeg进程为实时
+                    break;
+            }
         }
         private void ComPressor()
         {/*创建ffmpeg进程并监视*/
             for (int i = 0; i < listView1.Items.Count; i++)
             {//列表从上之下进行压缩
                 string path = listView1.Items[i].SubItems[0].Text;//临时存储预压缩的文件路径
-                if (File.Exists("E:\\A\\" + Path.GetFileName(path)))//目标文件已存在
-                {//检测E:\A\下是否已存在同名文件，若存在则询问覆盖或跳过
+                this.Text = i + " / " + listView1.Items.Count;//设置标题栏标记总进度
+                if (File.Exists(OutputFolder + "/" + Path.GetFileName(path)))//目标文件已存在
+                {//检测输出文件夹下是否已存在同名文件，若存在则询问覆盖或跳过
                     if (DialogResult.No == MessageBox.Show(null, "文件已经存在于输出文件夹中\n是：覆盖生成，否：跳过此文件", "文件重复", MessageBoxButtons.YesNo))
                     {//用户选择跳过，标记为complete并执行下一条压缩
                         listView1.Items[i].SubItems[1].Text = "Complete";
@@ -43,7 +70,7 @@ namespace MyCompressor
                         continue;
                     }
                     else//用户选择覆盖，删除原有同名文件
-                        File.Delete("E:\\A\\" + Path.GetFileName(path));
+                        File.Delete(OutputFolder + "/" + Path.GetFileName(path));
                 }
                 proc = new Process();//初始化进程
                 proc.StartInfo.FileName = "ffmpeg.exe";//目标为同目录下的ffmpeg
@@ -51,25 +78,22 @@ namespace MyCompressor
                 proc.StartInfo.CreateNoWindow = true;//静默无窗体显示
                 proc.StartInfo.RedirectStandardError = true;//重定向标准错误输出流
                 proc.StartInfo.Arguments = "-i " + path + //输入path
-                                     " -vcodec h264 -s 480*360 -b:v 384k " + //视频部分转换为h264 x360 动态码率 平均384k
-                                     "E:\\A\\" + Path.GetFileName(path);//输出为E:\A\下同名文件
+                                           " -vcodec h264 -s 480*360 -b:v 384k " + //视频部分转换为h264 x360 动态码率 平均384k
+                                           OutputFolder + "/" + Path.GetFileName(path);//输出为输出文件夹下同名文件
                 proc.ErrorDataReceived += new DataReceivedEventHandler(FFmpeg_Output);//输出流附加到FFmpeg_Output上
                 proc.Start();//启动ffmpeg进程，开始转换
+                SetPriorityByTrackBar();//设置FFmpeg进程优先值
                 listView1.Items[i].SubItems[1].Text = "Compressing";//并标记本条状态为压缩中
                 listView1.Items[i].BackColor = Color.Gold;
                 proc.BeginErrorReadLine();//开始读取错误输出流
                 proc.WaitForExit();//等待ffmpeg进程退出
-                proc.Close();//关闭进程
-                proc.Dispose();//清理进程
                 listView1.Items[i].SubItems[1].Text = "Complete";//标记本条转换状态为结束
                 listView1.Items[i].BackColor = Color.LawnGreen;
             }
-            button1.Enabled = true;
-            button1.Visible = true;
-            progressBar1.Visible = false;
-            button2.Enabled = false;
-            button2.Text = "暂停";
-            button3.Enabled = false;//各项控件恢复正常
+            proc.Close();//关闭进程
+            proc.Dispose();//清理进程
+            proc = null;
+            Wnd_AllEnd();//转换正常结束，各项控件重设
         }
         private int GetDuration(string Line)
         {/*返回获得的视频时长*/
@@ -98,55 +122,51 @@ namespace MyCompressor
             }
         }
         private void button1_Click(object sender, EventArgs e)
-        {
-
+        {/*按下开始按钮*/
             button1.Enabled = false;
-            button1.Visible = false;
-            progressBar1.Visible = true;
+            button1.Visible = false;//开始按钮无效并隐藏
+            progressBar1.Visible = true;//显示进度条
             button2.Enabled = true;
             button2.Text = "暂停";
-            button3.Enabled = true;
+            button3.Enabled = true;//暂停与停止按钮有效并重设暂停按钮
             th = new Thread(ComPressor);
-            th.IsBackground = true;
-            th.Start();
+            th.IsBackground = true;//创建后台线程
+            th.Priority = ThreadPriority.Highest;//监听线程最高优先级
+            th.Start();//启动线程
         }
         private void button2_Click(object sender, EventArgs e)
-        {
+        {/*按下暂停按钮，通过该按钮的Text标记当前状态*/
             if (button2.Text.Equals("暂停"))
-            {
-                NtSuspendProcess(proc.Handle);
-                for (int i = 0; i < listView1.Items.Count; i++)
+            {//若当前是转换状态
+                NtSuspendProcess(proc.Handle);//挂起FFmpeg进程
+                for (int i = 0; i < listView1.Items.Count; i++)//将列表中状态为转换的修改为暂停
                     if (listView1.Items[i].SubItems[1].Text.Equals("Compressing"))
                     {
                         listView1.Items[i].SubItems[1].Text = "Pause";
                         listView1.Items[i].BackColor = Color.Red;
                     }
-                button2.Text = "继续";
+                button2.Text = "继续";//更新按钮标记
             }
             else
-            {
-                NtResumeProcess(proc.Handle);
-                for (int i = 0; i < listView1.Items.Count; i++)
+            {//若当前是暂停状态
+                NtResumeProcess(proc.Handle);//恢复FFmpeg进程
+                for (int i = 0; i < listView1.Items.Count; i++)//将列表中状态为暂停的修改为转换
                     if (listView1.Items[i].SubItems[1].Text.Equals("Pause"))
                     {
                         listView1.Items[i].SubItems[1].Text = "Compressing";
                         listView1.Items[i].BackColor = Color.Gold;
                     }
-                button2.Text = "暂停";
+                button2.Text = "暂停";//更新按钮标记
             }
         }
         private void button3_Click(object sender, EventArgs e)
-        {
-            th.Abort();
-            proc.Kill();
-            button1.Enabled = true;
-            button1.Visible = true;
-            progressBar1.Visible = false;
-            button2.Enabled = false;
-            button2.Text = "暂停";
-            button3.Enabled = false;
-            listView1.Enabled = true;
-            for (int i = 0; i < listView1.Items.Count; i++)
+        {/*按下停止按钮*/
+            th.Abort();//终止监听线程
+            proc.Kill();//结束FFmpeg进程
+            proc.Dispose();//清理FFmpeg进程
+            proc = null;
+            Wnd_AllEnd();//窗体各控件重设
+            for (int i = 0; i < listView1.Items.Count; i++)//将列表中状态为转换与暂停的设为Ready
                 if (listView1.Items[i].SubItems[1].Text.Equals("Compressing") ||
                     listView1.Items[i].SubItems[1].Text.Equals("Pause"))
                 {
@@ -155,11 +175,11 @@ namespace MyCompressor
                 }
         }
         private void listView1_DragDrop(object sender, DragEventArgs e)
-        {
-            String[] Files = (String[])e.Data.GetData(DataFormats.FileDrop);
+        {/*列表拖拽松开事件*/
+            String[] Files = (String[])e.Data.GetData(DataFormats.FileDrop);//Files数组存放所有拖拽入的文件路径
             for (int i = 0; i < Files.Length; i++)
-            {
-                if (System.IO.Path.GetExtension(Files[i]) == ".mp4")
+            {//遍历Files路径并加入listView
+                if (System.IO.Path.GetExtension(Files[i]) == ".mp4")//只加入后缀名为.mp4的文件
                 {
                     listView1.Items.Add(new ListViewItem(new string[] { Files[i], "Ready" }));
                     listView1.Items[listView1.Items.Count-1].BackColor = Color.Red;
@@ -167,17 +187,47 @@ namespace MyCompressor
             }
         }
         private void listView1_DragEnter(object sender, DragEventArgs e)
-        {
+        {/*列表拖拽到边界事件*/
             if (button1.Enabled == true && e.Data.GetData(DataFormats.FileDrop) != null)
-            {
+            {//当且仅当准备状态才可向列表中拖拽文件，否则无效
                 e.Effect = DragDropEffects.Copy;
             }
         }
         private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
-        {
-            int index = listView1.Items.IndexOf(listView1.FocusedItem);
-            if (button1.Enabled == true && index != -1)
+        {/*列表项目双击事件*/
+            int index = listView1.Items.IndexOf(listView1.FocusedItem);//获取双击项的下标值
+            if (button1.Enabled == true && index != -1)//当且仅当准备状态才可双击删除，否则无效
                 listView1.Items.RemoveAt(index);
+        }
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {/*trackBar值变动事件*/
+            if (proc != null && proc.HasExited == false)
+            {//当且仅当进程存在并且进程并未退出的情况下设置优先级
+                SetPriorityByTrackBar();
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {/*窗体关闭前事件*/
+            if (button1.Enabled == false)//若当前状态为转换或暂停则拒绝关闭
+            {
+                MessageBox.Show(null,"关闭程序前请停止当前任务！","有任务正在执行");
+                e.Cancel = true;//取消关闭
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {/*主窗体加载事件*/
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            FBD.Description = "使用前请先选择输出文件夹，否则程序自动退出！\n请确保空间足够大！";
+            if (DialogResult.OK == FBD.ShowDialog())
+            {//确认选择
+                OutputFolder = FBD.SelectedPath;//保存输出文件夹
+            }
+            else
+            {//未选择
+                MessageBox.Show(null, "用户未指定输出位置，自动退出！", "输出文件夹无效");
+                this.Close();
+                this.Dispose();
+            }
         }
     }
 }
